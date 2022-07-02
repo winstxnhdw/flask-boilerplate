12ae29eff472af99de9047ae91574beead6a2e7d
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-07-02 22:55:50 +0800

    update debug print

--- app/libs/debug.py
+++ app/libs/debug.py
@@ -1,4 +1,9 @@
-"""
-Prints text to stdout.
-"""
-debug_print = lambda text: print(text, flush=True)
+from builtins import print as _print
+
+# Prints text to stdout.
+def print(*args, **kwargs):
+
+    if "flush" in kwargs:
+        del kwargs["flush"]
+
+    _print(*args, **kwargs, flush=True)
